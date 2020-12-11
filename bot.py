7b4cc46d6843d550150066f272f1d7b9b9ba2922
Timber <lucasbunel@gmail.com>
2020-12-11 16:03:59 +0100

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,19 @@
+import discord
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('!hello'):
+            await message.channel.send('Hello {0.author.mention}'.format(message))
+
+client = MyClient()
+client.run('Nzg2MTMyMzIzOTQ2MDcwMDI3.X9B8xw.dTqesr_wYhEF7Jq54Ddnvt5eNlM')
